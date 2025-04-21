7a4b33378a12a6b23f99b723466deed2cb5752c8
kushalbellenoru <kushalkumarm380@gmail.com>
2025-04-21 16:38:37 +0000

    Locators added

---
+++ Project/src/test/java/uistore/HomeDepotFooterLocator.java
@@ -0,0 +1,12 @@
+package uistore;
+
+import org.openqa.selenium.By;
+
+public class HomeDepotFooterLocator {
+    public static By CustomerServiceCenter = By.cssSelector("div[class='sui-w-full sui-flex sui-grid sui-grid-cols-4 sui-gap-3'] > div:nth-child(1)>ul>li:nth-child(1)>a");
+    public static By CheckOrderStatus = By.cssSelector("div[class='sui-w-full sui-flex sui-grid sui-grid-cols-4 sui-gap-3'] > div:nth-child(1)>ul>li:nth-child(2)>a");
+    public static By PayYourCreditCard = By.cssSelector("div[class='sui-w-full sui-flex sui-grid sui-grid-cols-4 sui-gap-3'] > div:nth-child(1)>ul>li:nth-child(3)>a");
+    public static By OrderCancellation = By.cssSelector("div[class='sui-w-full sui-flex sui-grid sui-grid-cols-4 sui-gap-3'] > div:nth-child(1)>ul>li:nth-child(4)>a");
+    public static By ReturnPolicy = By.cssSelector("div[class='sui-w-full sui-flex sui-grid sui-grid-cols-4 sui-gap-3'] > div:nth-child(1)>ul>li:nth-child(5)>a");
+    public static By RefundPolicy = By.cssSelector("div[class='sui-w-full sui-flex sui-grid sui-grid-cols-4 sui-gap-3'] > div:nth-child(1)>ul>li:nth-child(6)>a");
+}
